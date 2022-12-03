c478bf4e772678410ac074200cd22a653f77cecc
Jhônatan Reis <devjhonatanreis@gmail.com>
2022-12-03 10:17:44 -0300

    Inserindo Atualização no codigo

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav--menu');
+const NavMenu = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
-
 })
